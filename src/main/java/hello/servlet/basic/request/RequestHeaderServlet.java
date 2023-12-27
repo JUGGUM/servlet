e2e7d8a63b3aea8cc257ce8226de563c94d50b83
JUGGUM <gayoung801@naver.com>
2023-12-27 09:15:49 +0900

    Feat : RequestHeaderServlet.java

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
+    //HttpServletRequest http 요청하는 스펙을 편리하게 조회하기 위함
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         printStartLine(request);
