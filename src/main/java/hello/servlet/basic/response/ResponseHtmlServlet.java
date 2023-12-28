e77b8568be2141cdd857a61184ec2a7360ec337d
JUGGUM <gayoung801@naver.com>
2023-12-28 09:21:35 +0900

    Feat : ResponseHtmlServlet.java

---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: text/html;charset=utf-8
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println(" <div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+        // 코드로보면 웹 브라우저가 정확히 html을 렌더링 한것으로 볼수있다.
+    }
+}
