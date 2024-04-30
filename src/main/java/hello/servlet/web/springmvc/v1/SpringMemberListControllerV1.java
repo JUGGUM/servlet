e49f091722545f5169d665c70db71434b211df6c
JUGGUM <gayoung801@naver.com>
2024-04-30 09:32:17 +0900

    Feat : SpringMemberListControllerV1.java

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Controller
 public class SpringMemberListControllerV1 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
-    @RequestMapping("/springmvc/v1/members")
+    @RequestMapping("/springmvc/v1/members") // GET으로 와도되고 POST로도 와도되는데 이것은 좋은 방법이아니다.
     public ModelAndView process() {
         List<Member> members = memberRepository.findAll();
         ModelAndView mv = new ModelAndView("members");
